cb25b16e5af76411c2a1211d8b06fb7ffb663941
TQTos <tranquangtuyen135305@gmail.com>
2026-03-01 16:29:22 +0700

    fix: resolve import path

--- src/modules/payments/payments.module.ts
+++ src/modules/payments/payments.module.ts
@@ -2,26 +2,26 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from '../auth/entities/user.entity';
+import { MailModule } from '../mail';
+import { NotificationsModule } from '../notifications/notifications.module';
 import { PaymentsController } from './controllers/payments.controller';
+import { SubscriptionFeaturesController } from './controllers/subscription-features.controller';
 import { SubscriptionPlansController } from './controllers/subscription-plans.controller';
-import { PaymentMethod } from './payment-method.entity';
 import { PaymentMethodTranslation } from './entities/payment-method-translation.entity';
+import { PaymentMethod } from './entities/payment-method.entity';
 import { PaymentTransaction } from './entities/payment-transaction.entity';
+import { SubscriptionFeatureTranslation } from './entities/subscription-feature-translation.entity';
+import { SubscriptionFeature } from './entities/subscription-feature.entity';
 import { SubscriptionPlanTranslation } from './entities/subscription-plan-translation.entity';
 import { SubscriptionPlan } from './entities/subscription-plan.entity';
-import { VnPayProvider } from './providers/vnpay.provider';
 import { PaymentProviderFactory } from './providers/payment-provider.factory';
+import { VnPayProvider } from './providers/vnpay.provider';
 import { ExchangeRateService } from './services/exchange-rate.service';
 import { PaymentMethodService } from './services/payment-method.service';
 import { PaymentSubscriptionService } from './services/payment-subscription.service';
 import { PaymentsService } from './services/payments.service';
-import { SubscriptionPlansService } from './services/subscription-plans.service';
-import { MailModule } from '../mail';
-import { NotificationsModule } from '../notifications/notifications.module';
-import { SubscriptionFeature } from './entities/subscription-feature.entity';
-import { SubscriptionFeatureTranslation } from './entities/subscription-feature-translation.entity';
 import { SubscriptionFeatureService } from './services/subscription-feature.service';
-import { SubscriptionFeaturesController } from './controllers/subscription-features.controller';
+import { SubscriptionPlansService } from './services/subscription-plans.service';
 
 @Module({
   imports: [
