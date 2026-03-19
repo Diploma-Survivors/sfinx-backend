fe19cb687cb506756df0760534000979d63287e4
TQTos <tranquangtuyen135305@gmail.com>
2026-03-19 16:41:31 +0700

    feat(ci/cd): adopt reusable CI/CD workflows and DB URL

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -1,21 +1,34 @@
-import { DataSource, DataSourceOptions } from 'typeorm';
+import { DataSource } from 'typeorm';
+import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions';
 import { config } from 'dotenv';
 import { join } from 'path';
 
 config();
 
-export const dataSourceOptions: DataSourceOptions = {
-  type: 'postgres',
-  host: process.env.DB_HOST,
-  port: Number.parseInt(process.env.DB_PORT!, 10),
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+const baseOptions: Omit<PostgresConnectionOptions, 'type'> = {
   entities: [join(__dirname, '..', '**', '*.entity{.ts,.js}')],
   migrations: [join(__dirname, 'migrations', '*{.ts,.js}')],
   synchronize: false,
   logging: process.env.DB_LOGGING === 'true' || false,
 };
+
+export const dataSourceOptions: PostgresConnectionOptions = process.env
+  .DATABASE_URL
+  ? {
+      type: 'postgres',
+      url: process.env.DATABASE_URL,
+      ssl: { rejectUnauthorized: false },
+      ...baseOptions,
+    }
+  : {
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: Number.parseInt(process.env.DB_PORT!, 10),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      ...baseOptions,
+    };
 
 const dataSource = new DataSource(dataSourceOptions);
 
